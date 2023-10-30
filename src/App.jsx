21b0a80a9278d739400b60c89d202105de204e2d
AntonovAtanas <atanas.antonov1@gmail.com>
2023-10-30 15:55:56 +0200

    Add form for adding new friend

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,8 @@ function App() {
   return (
     <div className='app'>
       <FriendsList />
-      <AddFriend />
       <Bill />
+      <AddFriend />
     </div>
   )
 }
--- src/components/friends/Add-Friend.jsx
+++ src/components/friends/Add-Friend.jsx
@@ -2,8 +2,15 @@
 export default function AddFriend () {
 
     return (
-        <div>
-            
+        <div className="add-form-wrapper">
+            <form action="" className="form-add-friend">
+                <label htmlFor="">👩🏻‍🤝‍🧑🏼Friend name</label>
+                <input type="text" />
+                <label htmlFor="">📷 Image URL</label>
+                <input type="text" />
+                <button className="button">Add</button>
+            </form>
+                <button className="button form-toggle">Close</button>
         </div>
     )
 }
